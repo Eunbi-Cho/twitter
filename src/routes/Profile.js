06dd49fd479ad68145671a2598c0c90342025666
Anna <jeb731@naver.com>
2022-02-02 09:02:30 +0900

    catch the error

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { authService } from "../fbase";
 
 export default ({ refreshUser, userObj }) => {
